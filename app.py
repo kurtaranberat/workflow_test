472d0fe372264888cf33296653a49986e9d69061
Berat KURTARAN <99169158+kurtaranberat@users.noreply.github.com>
2025-03-29 16:59:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return " Docker Hub'a Hoş Geldiniz!!! update deneme "
+    return " Docker Hub'a Hoş Geldiniz!!! update deneme bilmem kac  "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
